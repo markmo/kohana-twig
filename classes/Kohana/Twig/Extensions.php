8950f8aefc63ede702bbdce107b4c1354e4dbfed
Mark Moloney <markmo@me.com>
2012-12-09 20:13:02 +1100

    add kohana helper

--- classes/Kohana/Twig/Extensions.php
+++ classes/Kohana/Twig/Extensions.php
@@ -24,6 +24,7 @@ class Kohana_Twig_Extensions extends Twig_Extension
 			new Kohana_Twig_Cache_TokenParser(),
 			new Kohana_Twig_Trans_TokenParser(),
 			new Kohana_Twig_Request_TokenParser(),
+			new Kohana_Twig_Kohana_TokenParser(),
 		);
 	}
 	
